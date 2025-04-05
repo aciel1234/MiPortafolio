f941b9135a23d82d9db23b6e3da7efd36f71a2e5
Aciel Olivera <acielito64@gmail.com>
2025-04-05 04:28:45 +0200

    nuevo proyecto

--- projects/tic-tac-toe/script.js
+++ projects/tic-tac-toe/script.js
@@ -10,45 +10,45 @@ const combinaciones = [
     [0,4,5],
     [2,4,6],
 ]
+
+let player1 = []
+let player2 = []
+
 for (let i = 0; i < 9; i++) {
     let cells = document.createElement('div');
     cells.classList.add('cells');
     container.appendChild(cells);
 }
 
+for (let i = 0; i < 9; i++) {
 container.addEventListener('click', (event) => {
     let clickedCell = event.target;
     if (clickedCell.classList == 'cells' && clickedCell.textContent == '' && playerTurn == 0) {
         clickedCell.textContent = 'X';
-        playerTurn = 1;
+        playerAdd(i);
+        playerTurn = 1;  
     } else if (clickedCell.classList == 'cells' && clickedCell.textContent == '' && playerTurn == 1) {
         clickedCell.textContent = 'O';
         playerTurn = '0';
     }
-})
 
+    checkWinner()
+})
+}
+
+function playerAdd(i) {
+    if (playerTurn == 0) {
+        player1.push(i);
+    }
+}
+ 
 function checkWinner() {
     combinaciones.find((item) => {
-        if (item.filter((i)=> container.includes(i))) {
-            let confirmar = confirm('El jugador 1 ha ganado. ¿Desea comenzar un nuevo juego?');
-                if (confirmar == true) {
-                    newGame()
-                }
-            return item
-        } else if (item.filter((i)=> container.includes(i))) {
-            let confirmar = confirm('El jugador 2 ha ganado. ¿Desea comenzar un nuevo juego?');
-            if (confirmar == true) {
-                newGame()
-            }
-            return
+        if (item.filter((i)=> player1.includes(i)).length === 3) {
+            alert('asd')
         }
     })
 }
-
- 
- 
- 
- 
  
  
  /* let cell = document.querySelectorAll('.cell');
